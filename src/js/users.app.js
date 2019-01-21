794ad7cd90ecb2531f354c7ca3b0563fb4d26138
Caleb Nordgren <cdn841@gmail.com>
2019-01-21 16:16:55 -0600

    added user index/creation views

--- src/js/users.app.js
+++ src/js/users.app.js
@@ -32,8 +32,8 @@ var usersApp = (function() {
         </tr>`;
       }
 
-      //Create a users panel, add a table to the panel, inject the rows into the
-      //table
+      //Create a users panel, add a table to the panel,
+      //inject the rows into the table
       table = `<div class="card">
         <div class="card-header clearfix">
           <h3 class="h3 float-left">Users</h3>
@@ -60,12 +60,89 @@ var usersApp = (function() {
     }
   }
 
+  function createUser(){
+    var app = document.getElementById('app');
+
+    var form =  `
+        <div class="card">
+          <div class="card-header clearfix">
+            <h2 class="h3 float-left">Create a New User</h2>
+            <div class="float-right">
+              <a href="#" class="btn btn-primary">Cancel</a>
+            </div>
+          </div>
+          <div class="card-body">
+            <form id="registrationForm" class="card-body">
+              <div id="formMsg" class="alert alert-danger text-center">Your form has errors</div>
+
+              <div class="row">
+                <div class="form-group col-md-6">
+                  <label for="first_name">First Name</label>
+                  <input type="text" id="first_name" name="first_name" class="form-control" required>
+                </div>
+
+                <div class="form-group col-md-6">
+                  <label for="last_name">Last Name</label>
+                  <input type="text" id="last_name" name="last_name" class="form-control" required>
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="form-group col-md-6">
+                  <label for="username">Username</label>
+                  <input type="text" id="username" name="username" class="form-control" required>
+                </div>
+
+                <div class="form-group col-md-6">
+                  <label for="email">Email</label>
+                  <input type="email" id="email" name="email" class="form-control" required>
+                </div>
+              </div>
+
+              <div class="text-right">
+                <input type="submit" value="Submit" class="btn btn-lg btn-primary btn-sm-block">
+              </div>
+            </form>
+          </div>
+        </div>
+    `;
+
+    app.innerHTML=form;
+  }
+
   return {
     load: function(){
-      viewUsers();
+      let hash = window.location.hash;
+      let hashArray = hash.split('-');
+
+      switch(hashArray[0]){
+        case '#create':
+          createUser();
+          break;
+
+        case '#view':
+          console.log('VIEW');
+          break;
+
+        case '#edit':
+          console.log('EDIT');
+          break;
+
+        case '#delete':
+          console.log('DELETE');
+          break;
+
+        default:
+          viewUsers();
+          break;
+      }
     }
   }
 
 })();
 
 usersApp.load();
+
+window.addEventListener("hashchange", function(){
+  usersApp.load();
+});
